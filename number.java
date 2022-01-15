bc6b784a9319877db3b463acd4c4f2028542c926
VishalChilap <97787821+VishalChilap@users.noreply.github.com>
2022-01-15 17:03:42 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,19 @@
+package gretestnumber;
+
+public class number {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		if(a>b) {
+			System.out.println(a);
+		}else if(b>c) {
+			System.out.println(b);
+		}else {
+			System.out.println(c);
+		}
+
+	}
+
+}
